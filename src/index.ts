58a31b2261967448ed022583f1ac3731ed27b0b7
arturgorsky <gorski.artur1@gmail.com>
2020-09-05 12:36:49 +0200

    Added express

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,21 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
+// import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
+import express from 'express';
 
 const main = async() => {
     const orm = await MikroORM.init(microConfig);
-    orm.getMigrator().up();
-    // const post = orm.em.create(Post, {title: 'my first post'});
-    // orm.em.persistAndFlush(post);
+    await orm.getMigrator().up();
+    const app = express();
 
-    // const posts = await orm.em.find(Post, {});
-    // console.log(posts);
+    app.get('/', (_, res) => {
+        res.send("Hello");
+    })
+
+    app.listen(4000, () => {
+        console.log('Server started on localhost: 4000');
+    })
     
 }
 
