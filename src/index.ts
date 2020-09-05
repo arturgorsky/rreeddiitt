9bbfe26db3e78b432c898b39c6a25c62cfd92355
arturgorsky <gorski.artur1@gmail.com>
2020-09-05 13:04:58 +0200

    GraphQL server setup, with typegraphQL schema

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,23 @@ import { __prod__ } from "./constants";
 // import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
 import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import {buildSchema} from 'type-graphql'
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async() => {
     const orm = await MikroORM.init(microConfig);
     await orm.getMigrator().up();
     const app = express();
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver],
+            validate: false
+        }),
+    });
+
+    apolloServer.applyMiddleware({app});
 
     app.get('/', (_, res) => {
         res.send("Hello");
